ba1bf06fc05ccb040cea66b82f8d82a9f0ff0013
Aleksi Uusitalo <aleksi.uusitalo@siili.com>
2019-12-14 00:54:07 +0200

    add unit tests to test business logic

---
+++ tests/billing.test.js
@@ -0,0 +1,28 @@
+import { calculateCost } from "../libs/billing-lib";
+
+test("Storage for less or equal to 10 notes", () => {
+  const storage = 10;
+
+  const cost = 4000;
+  const expectedCost = calculateCost(storage);
+
+  expect(cost).toEqual(expectedCost);
+});
+
+test("Storage for less or equal to 100 notes", () => {
+  const storage = 100;
+
+  const cost = 20000;
+  const expectedCost = calculateCost(storage);
+
+  expect(cost).toEqual(expectedCost);
+});
+
+test("Storage for more that 100 notes", () => {
+  const storage = 101;
+
+  const cost = 10100;
+  const expectedCost = calculateCost(storage);
+
+  expect(cost).toEqual(expectedCost);
+});
