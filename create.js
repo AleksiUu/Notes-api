915f24c1154603f1859dba250be5823337e6ba31
Aleksi Uusitalo <aleksi.uusitalo@siili.com>
2019-12-14 02:20:25 +0200

    remove debug log

--- create.js
+++ create.js
@@ -19,7 +19,6 @@ export const main = async (event, context) => {
     await dynamoDbLib.call("put", params);
     return success(params.Item);
   } catch (e) {
-    console.log(e);
     return failure({ status: false });
   }
 };
